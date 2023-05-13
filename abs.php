a1511259a541647f90a1a12cf4364c2d5138df71
Riz <inghugrizaldo5@gmail.com>
2023-05-13 13:24:42 +0800

    first commit

---
+++ abs.php
@@ -0,0 +1,22 @@
+<?php
+
+    abstract class Database 
+    {
+
+        public $conn;
+        public $server = "abstract";
+        public $user = "root";
+        public $pass = "";
+        public $dbname = "riz";
+        
+        public function __construct($conn)
+        {
+            $this->conn = new mysqli($this->server, $this->user, $this->pass);
+            $create = "CREATE DATABASE IF NOT EXISTS $this->dbname";
+            var_dump($this->conn);
+        }
+
+        abstract public function insert() : string;
+        
+    }
+?>
